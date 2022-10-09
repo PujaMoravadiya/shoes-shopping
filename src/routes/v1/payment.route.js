1c2375f42d47074da0951f761447375febdb8f80
Puja Moravadiya <87019203+PujaMoravadiya@users.noreply.github.com>
2022-10-09 18:53:02 +0530

    Delete src/routes/v1 directory

--- src/routes/v1/payment.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { payment } = require('../../controllers/payment.controller')
-
-const { verifyToken, verifyTokenAdmin } = require('../../middlewares/auth')
-
-router.route('/payments').post(verifyToken, payment)
-
-module.exports = router
